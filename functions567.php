7c46cada7d9ede99f4fed348519c82d8a5086097
opsecx <108930243+opsecx@users.noreply.github.com>
2024-03-04 14:47:39 +0100

    First Commit

---
+++ functions567.php
@@ -0,0 +1,66 @@
+<?php
+
+
+function strip_all($in) {
+  $in = trim($in);
+  $in = stripslashes($in);
+  $in = htmlspecialchars($in);
+  return $in;
+}
+
+function validate_tnam1($in) {
+  if (empty($in)) {
+    return false;
+  }    
+  strip_all($in);	
+  if (strlen($in) <> 45) {return false;}	
+  $reg_tnam1 = '/tnam1[a-zA-Z0-9]{40}$/i';	
+  return preg_match($reg_tnam1, $in);
+}
+
+function validate_integer($in) {
+  if (empty($in)) {
+    return false;
+  }    
+  strip_all($in);
+  if (strlen($in) > 25) {return false;}
+  $reg_int = '/\d+$/i';
+  return preg_match($reg_int, $in);
+}
+
+function validate_proposal_id($in) {
+	return validate_integer($in);
+}
+
+function validate_double($in) {
+  if (empty($in)) {return false;}	
+  $in = strip_all($in);
+  if (strlen($in) > 25) {return false;}
+  $reg_double = '/^\d*\.?\d+$/i';
+  return preg_match($reg_double, $in);
+}
+
+function hyperlink_address($in, $mode_arg) {
+   $in = strip_all($in);	
+   $mode_arg = strip_all($mode_arg);
+   if (validate_tnam1($in) == false) {die('wrong internal parameter call');}
+  $uri = strip_all($_SERVER['REQUEST_URI']);
+ $uri = array_shift(explode('?', $uri)); 
+  $servername = strip_all($_SERVER['HTTP_HOST']);
+  $newlink = 'https://' . $servername . $uri . '?address=' . $in . '&mode=' . $mode_arg . '&threshold=&ltgt=gt';
+  $returnstring = '<a href="' . $newlink . '" style="text-decoration: none">' . $in . '</a>'; 
+  return $returnstring;
+}
+
+function hyperlink_proposal($in, $mode_arg) {
+  $uri = strip_all($_SERVER['REQUEST_URI']);
+ $uri = array_shift(explode('?', $uri));
+  $servername = strip_all($_SERVER['HTTP_HOST']);
+  $newlink = 'https://' . $servername . $uri . '?proposal_id=' . $in;
+  $returnstring = '<a href="' . $newlink . '">' . $in . '</a>';
+  return $returnstring;
+}
+
+
+
+?>
